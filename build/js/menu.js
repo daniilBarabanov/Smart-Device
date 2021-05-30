930d8d4bfa4323e363e48e99d453794d8d4b103c
daniilBarabanov <55635367+daniilBarabanov@users.noreply.github.com>
2021-05-30 12:52:04 +0300

    8

--- build/js/menu.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('.page-header__burger').click(function(){
-        $('.page-header__burger').toggleClass('open-menu');
-        $('.page-header__nav').toggleClass('open-menu');
-        $('body').toggleClass('fixed-page');
-    });
-});
