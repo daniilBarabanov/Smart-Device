d1facb71fba84ed0ca642927e76569a4f0f9d69c
daniilBarabanov <55635367+daniilBarabanov@users.noreply.github.com>
2021-06-17 20:40:50 +0300

    dlya testa

--- build/js/main.js
+++ build/js/main.js
@@ -5,25 +5,29 @@
 (function () {
   var acc = document.getElementsByClassName('accordion');
   var i;
+  var allo = document.getElementsByClassName('allo');
+  var a;
+
+  for(a = 0; a < allo.length; a++) {
+    allo[a].classList.add('nonactive');
+    allo[a].style.display = 'none';
+    console.log('olo123');
+  }
 
   for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener('click', function () {
-      /* Toggle between adding and removing the "active" class,
-      to highlight the button that controls the panel */
       this.classList.toggle('active');
-      /* Toggle between hiding and showing the active panel */
-      for (let j = 0; j < acc.length; j++) {
-        if(j !== i) {
-          acc[j].nextElementSibling.style.display = 'none';
-        }
-      }
       var panel = this.nextElementSibling;
       if (panel.style.display === 'block') {
         panel.style.display = 'none';
-        evt.preventDefault();
       } else {
         panel.style.display = 'block';
-        evt.preventDefault();
+      }
+      for (let j = 0; j < acc.length; j++) {
+        if(j !== i) {
+          acc[j].nextElementSibling.style.display === 'none';
+          acc[j].nextElementSibling.style.maxHeight === '0';
+        }
       }
     });
   }
